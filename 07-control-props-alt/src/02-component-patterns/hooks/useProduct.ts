d35b24b7c87d5a959f42fa70dfef0a13b7a0271a
jhoan jimenez <jhoanjimz2@gmail.com>
2023-04-18 10:58:52 -0500

    curso completado hecho por Jhoan Jimenez

---
+++ 07-control-props-alt/src/02-component-patterns/hooks/useProduct.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { OnChangeArgs, Product } from "../interfaces/interfaces";
+
+interface UseProductArgs {
+  product: Product;
+  onChange?: (args: OnChangeArgs) => void; 
+  value?: number
+}
+
+export const useProduct = ({ onChange, product, value = 0 }: UseProductArgs) => {
+
+  const [counter, setCounter] = useState(value);
+
+  const increaseBy = (value: number) => {
+      
+    const newValue = Math.max( counter + value, 0 )
+    setCounter( newValue );
+
+    onChange && onChange({ count: newValue, product });
+  
+  }
+
+  useEffect(() => {
+    setCounter(value);
+  }, [value])
+  
+
+  return {
+    counter,
+    increaseBy
+  }
+}
