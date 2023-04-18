7e17c423d8cafbb1463762331b10cfaefa73d7d1
jhoan jimenez <jhoanjimz2@gmail.com>
2023-04-18 10:49:14 -0500

    curso completado hecho por Jhoan Jimenez

---
+++ 07-control-props/src/02-component-patterns/components/ProductCard.tsx
@@ -0,0 +1,34 @@
+import { ProductContextProps, Product, OnChangeArgs } from '../interfaces/interfaces';
+import { useProduct } from '../hooks/useProduct';
+import { ReactElement, createContext } from 'react';
+
+import styles from '../styles/styles.module.css';
+
+export const ProductContext = createContext({} as ProductContextProps);
+const { Provider } = ProductContext;
+
+
+export interface Props {
+  product: Product;
+  children?: ReactElement | ReactElement[];
+  className?: string;
+  style?: React.CSSProperties;
+  onChange?: ( args: OnChangeArgs ) => void;
+  value?: number;
+}
+
+export const ProductCard = ({ children, product, className, style, onChange, value }: Props) => {
+  
+  const { counter, increaseBy } = useProduct({ onChange, product, value });
+
+  return (
+    <Provider value={{ counter, increaseBy,product }}>
+      <div 
+        className={ `${ styles.productCard } ${ className }`}
+        style={ style }
+      >
+        { children }
+      </div>
+    </Provider>
+  )
+}
---
+++ 07-control-props/src/02-component-patterns/hooks/useShoppingCart.ts
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { Product, ProductInCart } from "../interfaces/interfaces";
+
+export const useShoppingCart = () => {
+
+    const [shoppingCart, setShoppingCart] = useState<{[key: string]: ProductInCart}>({});
+
+    const onProductCountChange = ({ count, product }:{ count: number, product: Product }) => {
+      setShoppingCart(oldShoppingCard => {
+        const productInCart: ProductInCart = oldShoppingCard[product.id] || { ...product, count: 0 };
+        if( Math.max( productInCart.count + count, 0 ) > 0 ) {
+          productInCart.count += count;
+          return {
+            ...oldShoppingCard,
+            [product.id]: productInCart
+          }
+        }
+        const { [product.id]: toDelete, ...rest } = oldShoppingCard;
+        return rest;
+      })
+    }
+
+    return {
+      shoppingCart,
+      onProductCountChange
+    }
+}
