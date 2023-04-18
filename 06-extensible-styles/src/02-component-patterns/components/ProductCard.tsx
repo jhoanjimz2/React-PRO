0769ad0062fb6ead791af906deb48aed95fbac7d
jhoan jimenez <jhoanjimz2@gmail.com>
2023-04-17 20:20:33 -0500

    curso completado hecho por Jhoan Jimenez

---
+++ 06-extensible-styles/src/02-component-patterns/components/ProductCard.tsx
@@ -0,0 +1,30 @@
+import { ProductContextProps, Product } from '../interfaces/interfaces';
+import { useProduct } from '../hooks/useProduct';
+import { ReactElement, createContext } from 'react';
+
+import styles from '../styles/styles.module.css';
+
+export const ProductContext = createContext({} as ProductContextProps);
+const { Provider } = ProductContext;
+
+
+export interface Props {
+  product: Product;
+  children?: ReactElement | ReactElement[];
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+export const ProductCard = ({ children, product, className, style }: Props) => {
+  const { counter, increaseBy } = useProduct();
+  return (
+    <Provider value={{ counter, increaseBy,product }}>
+      <div 
+        className={ `${ styles.productCard } ${ className }`}
+        style={ style }
+      >
+        { children }
+      </div>
+    </Provider>
+  )
+}
