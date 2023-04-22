468e075aad659b3b216784edc2a8436b4d24b01a
jhoan jimenez <jhoanjimz2@gmail.com>
2023-04-21 20:48:28 -0500

    curso completado hecho por Jhoan Jimenez

---
+++ 09-desplegar-paquete/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,36 @@
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components";
+import { products } from "../data/products";
+import '../styles/custom-styles.css';
+
+const product = products[0];
+
+export const ShoppingPage = () => {
+
+
+  return (
+    <div>
+        <h1>Shopping Store</h1>
+        <hr/>
+
+        <ProductCard 
+          key={product.id} 
+          product={ product } 
+          initialValues={{
+            count: 4,
+            // maxCount: 10
+          }}
+        >
+          {
+            ({ reset, increaseBy, isMaxCountReached, maxCount, count }) => (
+              <>
+                <ProductImage/>
+                <ProductTitle/>
+                <ProductButtons/>
+              </>
+            )
+          }
+        </ProductCard>
+
+    </div>
+  )
+}
