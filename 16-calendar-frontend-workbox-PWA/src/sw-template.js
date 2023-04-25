4056997d7da7aec3566b8a4ac17e2722a616fc87
jhoan jimenez <jhoanjimz2@gmail.com>
2023-04-24 22:36:40 -0500

    curso completado hecho por Jhoan Jimenez

--- 16-calendar-frontend-workbox-PWA/src/sw-template.js
+++ 16-calendar-frontend-workbox-PWA/src/sw-template.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-undef */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
-workbox.precaching.precacheAndRoute( [{"revision":"cf43be5e37d02dddceca41b04188fa28","url":"asset-manifest.json"},{"revision":"6e1267d9d946b0236cdf6ffd02890894","url":"favicon.ico"},{"revision":"27f979166976034275f48b2b287d8b8b","url":"index.html"},{"revision":"33dbdd0177549353eeeb785d02c294af","url":"logo192.png"},{"revision":"917515db74ea8d1aee6a246cfbcc0b45","url":"logo512.png"},{"revision":"258f5560d5a842ce46c4b09acf6ef8ba","url":"manifest.json"},{"revision":"fa1ded1ed7c11438a9b0385b1e112850","url":"robots.txt"},{"revision":"bfdc3ff354a76bada4870efecb3a5eb0","url":"static/css/main.a3574b48.css"},{"revision":"53a8e41d42079cc40e0db947a084024b","url":"static/js/main.45b486f4.js"},{"revision":"3cc2e497e60627dcaca6aebf4050715c","url":"static/js/main.45b486f4.js.LICENSE.txt"}] );
+workbox.precaching.precacheAndRoute( self.__WB_MANIFEST );
 
 const { BackgroundSyncPlugin } =workbox.backgroundSync;
 const { registerRoute } = workbox.routing;
